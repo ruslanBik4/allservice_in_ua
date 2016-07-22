1c16f40f2cf07d757c4303e9c7fc6ea85a9b5fa2
ruslan-bik <ruslan-bik@yandex.ru>
2016-07-22 11:23:12 +0300

    Добавил автолоад!

---
+++ autoload.php
@@ -0,0 +1,32 @@
+<?php
+    $ROOT_DIR = __DIR__ ;
+       
+    function __autoload($className)
+    {
+       global $ROOT_DIR;
+       
+       $pathInclude = [ 
+           'models',
+           'models/config',
+           'views',
+           'views/tables',
+           'views/forms',
+           'views/menus',
+           'controllers',
+           'isenka',
+           'isenka/bridge',
+           'investors',
+       ];
+       
+        
+       foreach($pathInclude as $path) {
+           
+          $nameFile = "$ROOT_DIR/$path/$className.php";
+          if (file_exists($nameFile)) {
+            include_once($nameFile);
+            break;
+          }
+     
+      }
+      
+    }
