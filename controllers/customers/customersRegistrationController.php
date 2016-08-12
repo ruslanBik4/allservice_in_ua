6e5a57163d9459a42bf461dde9bd5c7d0581b5c1
khvorostianenko <mikhail.khvorostianenko@gmail.com>
2016-08-13 00:09:42 +0300

    merge with Ruslan

--- controllers/customers/customersRegistrationController.php
+++ controllers/customers/customersRegistrationController.php
@@ -21,11 +21,8 @@ class customersRegistrationController {
         $query = "input_form_info('client_registration')";
         $json = $queryObject->callProcedure($query); 
         $form = new formCreatorFromJsonClass($json, $this->queryString);
-        $print = "<head><script src='../../web/js/registration_validate.js'></script>
-<script src='https://code.jquery.com/jquery-1.12.0.min.js'></script>
-<script src='https://code.jquery.com/jquery-migrate-1.2.1.min.js'></script></head><body>";
-        return "$print Для регистрации заполните форму:<br>Она отправиться на {$this->handler}<br>"
+        return "Для регистрации заполните форму:<br>Она отправиться на {$this->handler}<br>"
          . $form->formCreation($this->handler );
    }
     
- }
+ }  
