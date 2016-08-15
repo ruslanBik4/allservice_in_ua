fa159fe8cb5de5c8acb525427fffcd99f1ca003c
khvorostianenko <mikhail.khvorostianenko@gmail.com>
2016-08-15 16:11:01 +0300

    customersRegistrationController mod

--- controllers/customers/customersRegistrationController.php
+++ controllers/customers/customersRegistrationController.php
@@ -1,32 +1,44 @@
 <?php
 class customersRegistrationController {
 
-    private $handler;
-    private $queryString = [];
-    
-    public function __construct($handler, $queryString = null) {
-        $this->handler = $handler;
-        
-        if ($queryString)
-            $this->queryString = array_merge($this->queryString, $queryString);
-            
-            
-            
-    }
+    // значение атрибута action тега form
+    private $actionAttribute;
+
+    // массив GET
+    private $getParams = [];
 
     /**
-     * Метод для отрисовки формы регистрации
+     * Конструктор принимает $actionAttribute - значение атрибута action тега form,
+     * записывает в actionAttribute
+     * customersRegistrationController constructor.
+     * @param $actionAttribute
+     * @param null $getParams
+     */
+    public function __construct($actionAttribute, $getParams = null) {
+        $this->actionAttribute = $actionAttribute;
+        if ($getParams)
+            $this->getParams = array_merge($this->getParams, $getParams);
+    }
+
+
+    /**
+     * Метод извлекает данные требуемой таблицы и отправляет на создание в класс formCreatorClass
+     * Возвращает описание формы и HTML разметку самой формы для вставки на страницу
      * @return string
      */
     public function getFormRegistration() {
-        
-         // извлекает свойства полей таблиц для регистрации
-        $queryObject = new QueryOld();
+        $queryObject = new Query();
         $query = "input_form_info('client_registration')";
-        $json = $queryObject->callProcedure($query);
-        $form = new formCreatorFromJsonClass($json, $this->queryString);
-        return "Для регистрации заполните форму:<br>Она отправиться на {$this->handler}<br>"
-         . $form->formCreation($this->handler );
+
+        // получаем данные для рисования формы, вида
+        //array(16) {["action"],["constraint_name"],["constraint_value"],["db_field_name"],["db_table_name"],["form_html_id"],
+        //["form_html_name"],["html_class"],["html_id"],"html_name"],["html_placeholder"],["html_type"],["html_value"],
+        //["js_func_onsubmit"],["label"],["relative_html_input_name"]}
+        $data = $queryObject->callProcedure($query);
+
+        // в formCreatorClass передаем данные полей таблицы и массив GET
+        $form = new formCreatorClass($data, $this->getParams);
+        return "Для регистрации заполните форму:<br>Она отправиться на {$this->actionAttribute}<br>". $form->formCreation($this->actionAttribute );
    }
     
  }  
